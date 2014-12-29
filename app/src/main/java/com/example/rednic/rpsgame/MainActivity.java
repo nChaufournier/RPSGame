fc3e2193bae19ad83bd188423e48b30c78ca029b
Nic Chaufournier <rednic11@gmail.com>
2014-12-29 01:03:49 -0500

    Added a new comment

--- app/src/main/java/com/example/rednic/rpsgame/MainActivity.java
+++ app/src/main/java/com/example/rednic/rpsgame/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-
+//This is a test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
