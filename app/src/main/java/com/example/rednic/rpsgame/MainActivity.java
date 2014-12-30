b9e6661080d05dbbb1c5ec6716c31017ad55f306
nChaufournier <nchaufournier140@cardinal.wju.edu>
2014-12-30 00:28:05 -0800

    Added Images and intent to switch activities

--- app/src/main/java/com/example/rednic/rpsgame/MainActivity.java
+++ app/src/main/java/com/example/rednic/rpsgame/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.rednic.rpsgame;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import java.util.Random;
 
@@ -48,7 +48,10 @@ public class MainActivity extends ActionBarActivity {
         rockButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), Results.class);
+                startActivity(i);
                 Player = 0;
+
             }
         });
 
@@ -56,6 +59,8 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 Player = 1;
+                Intent i = new Intent(v.getContext(), Results.class);
+                startActivity(i);
             }
         });
 
@@ -63,6 +68,8 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 Player = 2;
+                Intent i = new Intent(v.getContext(), Results.class);
+                startActivity(i);
             }
         });
 
---
+++ app/src/main/java/com/example/rednic/rpsgame/Results.java
@@ -0,0 +1,15 @@
+package com.example.rednic.rpsgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Nic on 12/30/2014.
+ */
+public class Results extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.results);
+    }
+}
